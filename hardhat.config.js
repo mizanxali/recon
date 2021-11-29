ca1643845dfc07aeeac5fc27dfa2387679098464
Mizanali Panjwani <mizanalip786@gmail.com>
2021-11-29 21:09:02 +0530

    Hardhat config

---
+++ hardhat.config.js
@@ -0,0 +1,20 @@
+require('@nomiclabs/hardhat-waffle');
+const fs = require('fs');
+const privateKey = fs.readFileSync('.secret').toString();
+
+module.exports = {
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    mumbai: {
+      url: 'https://rpc-mumbai.matic.today',
+      accounts: [privateKey]
+    },
+    mainnet: {
+      url: 'https://rpc-mainnet.matic.network',
+      accounts: [privateKey]
+    }
+  },
+  solidity: '0.8.4',
+};
